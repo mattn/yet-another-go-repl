4f6cd978f73ef1c8c75aeac49bb6877e0677fb41
mattn <mattn.jp@gmail.com>
2010-10-26 18:44:03 +0900

    removed dependency of go-readline

--- go-repl.go
+++ go-repl.go
@@ -3,28 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
-	"readline"
+	"github.com/kless/go-readin/readin"
 	"exp/eval"
-);
+)
 
 func main() {
-	prompt := "go> ";
 	world := eval.NewWorld()
 	for {
-		line := readline.ReadLine(&prompt);
-		readline.AddHistory(*line);
-		code, err := world.Compile(*line + ";")
+		line, err := readin.Input("go> ")
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "go-repl: ", err.String());
+			fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
+			continue
+		}
+		code, err := world.Compile(line + ";")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
 			continue
 		}
 		ret, err := code.Run()
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "go-repl: ", err.String());
+			fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
 			continue
 		}
 		if ret != nil {
-			println(ret.String());
+			println(ret.String())
 		}
 	}
 }
