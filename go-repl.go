c1ec1cbccac3678a0be1a52c7381b8d4a5f83588
Simon Zimmermann <simonz05@gmail.com>
2011-02-19 13:37:20 +0000

    format source

--- go-repl.go
+++ go-repl.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"github.com/kless/go-readin/readin"
-	"exp/eval"
+    "fmt"
+    "os"
+    "github.com/kless/go-readin/readin"
+    "exp/eval"
     "go/token"
 )
 
 func main() {
-	world := eval.NewWorld()
+    world := eval.NewWorld()
     var fset = token.NewFileSet()
-	for {
-		line, err := readin.RepeatPrompt("go> ")
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
-			continue
-		}
-        code, err := world.Compile(fset, line + ";")
-		if err != nil {
-		 	fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
-		 	continue
-		}
-		ret, err := code.Run()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
-			continue
-		}
-		if ret != nil {
-			println(ret.String())
-		}
-	}
+    for {
+        line, err := readin.RepeatPrompt("go> ")
+        if err != nil {
+            fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
+            continue
+        }
+        code, err := world.Compile(fset, line+";")
+        if err != nil {
+            fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
+            continue
+        }
+        ret, err := code.Run()
+        if err != nil {
+            fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
+            continue
+        }
+        if ret != nil {
+            println(ret.String())
+        }
+    }
 }
