1350539cf1ecc5f27c98ffb0ae287b5b8fe34ab2
Simon Zimmermann <simonz05@gmail.com>
2011-02-19 13:36:10 +0000

    make it compile

--- go-repl.go
+++ go-repl.go
@@ -5,20 +5,22 @@ import (
 	"os"
 	"github.com/kless/go-readin/readin"
 	"exp/eval"
+    "go/token"
 )
 
 func main() {
 	world := eval.NewWorld()
+    var fset = token.NewFileSet()
 	for {
-		line, err := readin.Input("go> ")
+		line, err := readin.RepeatPrompt("go> ")
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
 			continue
 		}
-		code, err := world.Compile(line + ";")
+        code, err := world.Compile(fset, line + ";")
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
-			continue
+		 	fmt.Fprintln(os.Stderr, "go-repl: ", err.String())
+		 	continue
 		}
 		ret, err := code.Run()
 		if err != nil {
